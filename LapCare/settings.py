6b573ef993a994b9e91b9de54d14e20e41f37c42
krishnajith <krishnajithus673@gmail.com>
2022-10-12 14:50:59 +0530

    tkinder

--- LapCare/settings.py
+++ LapCare/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =env('SECRET_KEY')#to get the value of secret key fromn .env
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = [' LapCare-env.eba-8mbmj3k4.us-west-2.elasticbeanstalk.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from re import T
-from tkinter.tix import Tree
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
